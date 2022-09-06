99108541c03113b7137ca495cf29f1c45348754a
Ruslan Ivasyuk <r.ivasyuk@gmail.com>
2022-09-06 15:17:55 +0300

    add array description

--- main.dart
+++ main.dart
@@ -6,10 +6,20 @@ void main() {
 // all variables have a default value of null
 
   String name = "Paul";
+  var name2 = "Oksana";
+
   int age = 35;
   double temperature = 18.5;
 
-  var name2 = "Oksana";
-  
+  bool isOpen = true;
+  dynamic unknown;
+
+  // List - ordered array of dart
+  List<int> myList = [1, 2, 3];
+  var mySecondList = ["one", "two", "three"];
+  var list2 = [0, ...myList]; // spred operator
+
+  print(list2);
+
   print("Hello World");
 }
