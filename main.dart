a190f6ae28054782e8c01c226c69a3526de4caca
Ruslan Ivasyuk <r.ivasyuk@gmail.com>
2022-09-06 14:29:22 +0300

    annd first file

---
+++ main.dart
@@ -0,0 +1,15 @@
+//https://dartpad.dev/
+
+void main() {
+// Variables are object
+// in dart all are objects
+// all variables have a default value of null
+
+  String name = "Paul";
+  int age = 35;
+  double temperature = 18.5;
+
+  var name2 = "Oksana";
+  
+  print("Hello World");
+}
